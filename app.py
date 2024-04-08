931950a86a4f09597237dcf43c5708bef8f98f3c
Arthur Lewis <arthur.lewis420@gmail.com>
2024-04-08 14:10:52 +1200

    Added database settings for rds in app.py

--- app.py
+++ app.py
@@ -7,8 +7,20 @@ from flask_migrate import Migrate
 # Init App
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissecret'
+
 # our database uri
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Cellfone01@localhost:5432/contactsdb"
+if 'RDS_DB_NAME' in os.environ:
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'postgresql://{username}:{password}@{host}:{port}/{database}'.format(
+        username=os.environ['RDS_USERNAME'],
+        password=os.environ['RDS_PASSWORD'],
+        host=os.environ['RDS_HOSTNAME'],
+        port=os.environ['RDS_PORT'],
+        database=os.environ['RDS_DB_NAME'],
+    )
+else:
+    # our database uri
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Cellfone01@localhost:5432/contactsdb"
 
 db = SQLAlchemy()
 db.init_app(app)
